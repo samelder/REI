906a4f15826cca743eb6f204ae21f6e0d5cd610c
Samuel Elder <samueloelder@gmail.com>
2019-02-26 20:33:09 -0800

    Trie

---
+++ src/main/java/WordSearch/Main.java
@@ -0,0 +1,17 @@
+package WordSearch;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Trie trie = new Trie();
+        trie.insert("art");
+        trie.insert("bunny");
+        trie.insert("buck");
+        trie.insert("dog");
+        trie.insert("Apostate");
+        boolean res = trie.search("bunny");
+        System.out.println(res);
+    }
+
+}
